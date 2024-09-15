d74ed76471994009c4768d69648c054461f175c3
vanperdung <dung.nv382001@gmail.com>
2024-09-15 21:30:57 +0700

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade 
+
+sudo apt install zsh tmux xclip git
+
+sudo curl -L http://install.ohmyz.sh | sh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
