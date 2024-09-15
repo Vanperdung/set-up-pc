30249979f8095323150b0a04755bf3fb6b625885
vanperdung <dung.nv382001@gmail.com>
2024-09-15 21:41:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
-sudo apt-get update && sudo apt-get upgrade 
+sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt install zsh tmux xclip git
+sudo apt install zsh tmux xclip git -y
 
 sudo curl -L http://install.ohmyz.sh | sh
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp .tmux.conf ~/ && cp .zshrc ~/.
+
+tmux
+
 
