b1f0053360b12c335bcfecf5bc39df9cfc7e9308
Nguyen Van Dung <82029273+Vanperdung@users.noreply.github.com>
2024-11-01 17:24:31 +0700

    Update setup.sh

--- .zshrc
+++ .zshrc
@@ -78,7 +78,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 # plugins=(git)
-plugins=(zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-fzf-history-search)
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt install zsh tmux xclip git -y
+sudo apt install zsh tmux xclip git curl vim -y
 
 sudo curl -L http://install.ohmyz.sh | sh
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cp .tmux.conf ~/ && cp .zshrc ~/.
